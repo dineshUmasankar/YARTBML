650caa613298b0db9c0f7f64a8f9b9e6e31615c0
Joseph Porrino <joeyporrino1998@gmail.com>
2024-03-20 18:41:45 -0400

    Lexer tests and token struct

--- src/token/token.go
+++ src/token/token.go
@@ -1,1 +1,35 @@
 package token
+
+type TokenType String
+
+type Token struct {
+	Type TokenType
+	Literal string
+}
+
+// Symbolic names substituted at complile time for the assigned value
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 123456
+	
+	//operators
+	ASSIGN  = "="
+	PLUS    = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
