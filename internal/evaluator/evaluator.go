868311f77bcfee8ced55dc9224a34e9b3a750a6b
Joseph Porrino <joeyporrino1998@gmail.com>
2024-04-09 19:43:08 -0400

    Eval functionality and tests

---
+++ internal/evaluator/evaluator.go
@@ -0,0 +1,34 @@
+// Package evaluator provides functionality to interpret the AST output from the parser.
+// The evaluator follows the "tree-walking" method by recursively working its way through each node in the AST.
+
+package evaluator
+
+import (
+	"YARTBML/ast"
+	"YARTBML/object"
+)
+
+// Takes an AST node and outputs the corresponding object
+// Recusively calls Eval to "tree-walk" the AST
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	// Statements
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+	// Expressions
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	}
+	return nil
+}
+
+// Evaluates each statement in the list and return the last
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+	for _, statement := range stmts {
+		result = Eval(statement)
+	}
+	return result
+}
